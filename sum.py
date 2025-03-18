da85cd915ab19a54d1b554662657886b8a85cd44
alit <alitrose@karunya.edu.in>
2025-03-18 11:36:28 +0530

    first commit

---
+++ sum.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+a=5
+b=3
+c=a+b
+print(c)
+=======
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+sum=a+b
+print(sum)
+>>>>>>> aedfcdc07cdb6c6cfd4b89dd980abda3f2f48b45
